18ba54d0ec32914dcd9ba2982df7739bd441f789
jefinjose <98053675+jefinjose@users.noreply.github.com>
2022-04-12 15:11:54 -0400

    Create adap_information_lab3.ps1

---
+++ powershell/adap_information_lab3.ps1
@@ -0,0 +1,31 @@
+﻿Get-Ciminstance -ClassName win32_networkadapterconfiguration |
+where { $_.ipenabled -eq "true"}| 
+Format-Table -AutoSize Description, Index, IPAddress, IPSubnet,
+@{n="DNSServersearchorder";
+  e={Switch($_.DNSServersearchorder)
+    {
+      $NULL{
+        $variable1="No Data Found.";
+        $variable1
+    }
+   };
+   if($null -ne $_.DNSServerSearchorder)
+     {
+     $_.DNSServerSearchorder
+     }
+    }
+  },
+@{n="DNSdomain";
+  e={Switch($_.DNSdomain)
+    {
+      $NULL{
+        $variable2="No Data Found.";
+        $variable2
+    }
+   };
+   if($null -ne $_.DNSdomain)
+     {
+     $_.DNSdomain
+     }
+    }
+  }
